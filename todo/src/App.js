275c4015655b15e54459cbc79211592a6c35c84d
Ritesh Sharma <132354417+Ritesh-sviet@users.noreply.github.com>
2023-11-04 08:19:24 +0000

    modified:   todo/public/index.html 	modified:   todo/src/App.js

--- todo/src/App.js
+++ todo/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Visit my LinkedIn profile
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/ritesh-sharma-a22720281/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ritesh Sharma
         </a>
       </header>
     </div>
