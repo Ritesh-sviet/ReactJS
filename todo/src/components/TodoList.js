34314c99f9f1c074d0fcfdaaee1b29d3f7480efa
Ritesh Sharma <132354417+Ritesh-sviet@users.noreply.github.com>
2023-11-05 04:13:01 +0000

    modified:   todo/package-lock.json 	modified:   todo/package.json 	modified:   todo/public/index.html 	new file:   todo/src/components/TodoList.css 	modified:   todo/src/components/TodoList.js

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -3,11 +3,27 @@ import React from 'react'
 const TodoList = (props) => {
   return (
     <>
-        <>Welcome Mr. {props.User}</>
-        <h1>This is another file for TodoList</h1>
-        <p>Here we will create our List and other tasks</p>
-        <button onClick={() => props.setLoggedin(!props.Loggedin)}>Log Out</button>
-        {/* console.log(props.Loggedin); */}
+      <>Welcome Mr. {props.User}</>
+      <h1>This is another file for TodoList</h1>
+      <p>Here we will create our List and other tasks</p>
+      <button onClick={() => props.setLoggedin(!props.Loggedin)} variant="primary">Log Out</button>{' '}
+      {/* console.log(props.Loggedin); */}
+      <div className='ListTodos'>
+        <table>
+          <thead>
+            <th>Todos</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2</td>
+              <td>2</td>
+              <td>2</td>
+              <td>2</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
     </>
   )
 }
