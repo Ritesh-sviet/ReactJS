9cce66fbfc96695681a142ffda4c90b97075a428
Ritesh Sharma <132354417+Ritesh-sviet@users.noreply.github.com>
2023-11-07 16:51:06 +0530

    Update AddTodo.js

--- todo/src/components/AddTodo.js
+++ todo/src/components/AddTodo.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
 import Button from 'react-bootstrap/esm/Button';
 import './Addtodo.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 const AddTodo = (props) => {
+  const [title, settitle] = useState("");
+  const [Desc, setdesc] = useState("");
+  const [owner, setowner] = useState("");
+  useEffect(() => {
+    settitle(props?.Editor?.Title)
+    setdesc(props?.Editor?.Describe)
+    setowner(props?.Editor?.Creator)
+  }, [props.setEditor])
 
-  const AdditUp = (event) => { 
+  const AdditUp = (event) => {
     let Form = event.target;
     let Objective = Form.Add.value;
     let Desc = Form.desc.value;
@@ -12,59 +21,34 @@ const AddTodo = (props) => {
     event.preventDefault();
 
     if (Objective !== '' && Desc !== '') {
-      let tmp = { Title: Objective, Describe : Desc, Creator : Creator === '' ? "Owner": Creator };
+      let tmp = { Title: Objective, Describe: Desc, Creator: Creator === '' ? "Owner" : Creator };
+
       props.WhatToDo(tmp);
       props.setNorecord(true);
+      props.setuserid(props.userid + 1);
+      alert(props.userid + 1);
+
     }
-    else{
+    else {
       alert("You have to fill all the fields")
       props.setNorecord(false)
     }
+
   }
+
 
   return (
     <>
       <form onSubmit={AdditUp}>
-          <input type='text' id='Add' className='motive' placeholder='write✍ your ToDo Title here' onChange={(e) => e.target.value}/>
-          <br/>
-          <input type='text' id='desc' className='motive' placeholder='Describe✍ your Task here'/>
-          <br/>
-          <input type='text' id='owner' className='motive' placeholder='Enter your Good name'/>
-          
-          <Button type='submit' className='Add'>Add ➕</Button>
-        </form>
-      {/* <form onSubmit={AdditUp}>
-        <label htmlFor="Add">Title</label>
-        <input type="text" id="add" name="title" placeholder="Write your text here" />
-        
-        <label htmlFor="desc">Description</label>
-        <input type="text" id="Add" name="description" placeholder="Describe your todo" />
-        
-        <label htmlFor="task">Tasks Status</label>
-        <select id="Add" name="taskStatus">
-          <option value="--Select--">--Select--</option>
-          <option value="To Do">To Do</option>
-          <option value="In Progress">In Progress</option>
-          <option value="Completed">Completed</option>
-        </select>
+
+        <input type='text' id='Add' className='motive' placeholder='write✍ your ToDo Title here' value={title} onChange={T => settitle(T.target.value)} /> {/* First of all set the value blank of title and when the value is changed then a function will get an event and set the value of input*/}
         <br />
-        <label htmlFor="status">Status</label>
+        <input type='text' id='desc' className='motive' placeholder='Describe✍ your Task here' value={Desc} onChange={D => setdesc(D.target.value)} /> {/* Same as above */}
         <br />
-        
-        <input type="radio" id="Add" name="status" value="Active" />
-        <label>Active</label> 
-        <br/>
-        <input type="radio" id="Add" name="status" value="Deactive" />
-        <label>Deactive</label>
-        <br/>
-        <label htmlFor="due">Due Date</label>
-        <input type="text" id="Add" name="dueDate" placeholder="Enter the Due Date" />
+        <input type='text' id='owner' className='motive' placeholder='Enter your Good name' value={owner} onChange={O => setowner(O.target.value)} /> {/* Same as above */}
 
-        &nbsp;&nbsp;&nbsp;
-
-        <Button type="submit" className="Add">Add ➕</Button>
-      </form> */}
-
+        <Button type='submit' className='Add'>Add ➕</Button>
+      </form>
     </>
   )
 }
